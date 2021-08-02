1a63a7edd2274779ddfd32be02309021fb00b3ac
nealxun <nealxun@gmail.com>
2021-08-02 15:26:00 -0700

    regenerate PAT

--- github_setup.R
+++ github_setup.R
@@ -6,4 +6,11 @@ rm(list = ls())
 # create a remote on github for the existing project
 usethis::use_github()
 
+# generate PAT (personal access token)
+# https://github.com/settings/tokens
+
+# store your credential
+
+
+
 
